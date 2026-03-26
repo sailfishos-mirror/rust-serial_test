9ad7a8f18c9a598109df5197214669e8680ccb96
justanotheranonymoususer <justanotheranonymoususer@users.noreply.github.com>
2026-03-26 16:42:59 +0200

    Remove unnecessary test leftover changes

--- serial_test/tests/tests.rs
+++ serial_test/tests/tests.rs
@@ -1,18 +1,8 @@
-use serial_test::{local_parallel_core, local_serial_core};
+use serial_test::local_serial_core;
 
 #[test]
 fn test_empty_serial_call() {
     local_serial_core(vec!["beta"], None, || {
         println!("Bar");
     });
-}
-
-// Verify that serial_test is compatible with Miri's strict provenance mode.
-//
-// Run with:
-//   MIRIFLAGS="-Zmiri-strict-provenance" cargo +nightly miri test -p serial_test --no-default-features --test tests test_miri_strict_provenance
-#[test]
-fn test_miri_strict_provenance() {
-    local_serial_core(vec!["miri_provenance_test"], None, || {});
-    local_parallel_core(vec!["miri_provenance_test"], None, || {});
 }
